1b38c4fcfcaa706a72f93be4300ad911d0c4b7d3
Bapi Shaikh <bapishaikh1148@gmail.com>
2023-12-30 23:22:05 +0530

    added new features

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/Popular'
 import Offer from '../components/Offers/Offer'
+import NewCollections from '../components/NewCollections/NewCollections'
+import NewsLetter from '../components/NewsLetter/NewsLetter'
 
 const Shop = () => {
   return (
@@ -9,6 +11,8 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offer/>
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
