574bf8bc3c368d56e1c4b39da595ff61d57d91fc
Bapi Shaikh <bapishaikh1148@gmail.com>
2023-12-31 13:10:26 +0530

    added new files

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -8,9 +8,15 @@ const Product = () => {
     const {all_product} = useContext(ShopContext)
     const {productId} = useParams();
     const product = all_product.find((e)=>e.id ===Number(productId) )
+    
+    
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
   return (
     <div>
-      <Breadcrum product = {product}/>
+      <Breadcrum product= {product}/>
       <ProductDisplay product={product}/>
     </div>
   )
